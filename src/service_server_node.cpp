90860ca5c5d4c2449c1bd5ca9d2742faef7f95af
Chris Chun <arirang2067@naver.com>
2024-09-27 13:50:20 +0900

    안되는거 핫픽스

--- src/service_server_node.cpp
+++ src/service_server_node.cpp
@@ -3,7 +3,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "tutorial_interfaces/srv/add_two_ints.hpp"
 
-void add(const std::shared_ptr<tutorial_interfaces::srv::AddTwoInts::Request> request,
+void Add(const std::shared_ptr<tutorial_interfaces::srv::AddTwoInts::Request> request,
          std::shared_ptr<tutorial_interfaces::srv::AddTwoInts::Response> response)
 {
   response->sum = request->a + request->b;
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("tutorial_server");
 
   rclcpp::Service<tutorial_interfaces::srv::AddTwoInts>::SharedPtr service =
-      node->create_service<tutorial_interfaces::srv::AddTwoInts>("/tutorial/add_two_ints", &add);
+      node->create_service<tutorial_interfaces::srv::AddTwoInts>("/tutorial/add_two_ints", &Add);
 
   RCLCPP_INFO(rclcpp::get_logger("tutorial_server"), "Ready to add two ints.");
 
